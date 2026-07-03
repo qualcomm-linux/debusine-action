7c29ffc3233f3595b225df0362ef6c6ec809a6fc
Robie Basak <robie.basak@oss.qualcomm.com>
2026-07-03 13:20:35 +0100

    Add test for sub-bpo version bump
    If bumping 1.0.19+ds-0qli3~bpo13+1~ then the answer should be
    1.0.19+ds-0qli3~bpo13+2~ and not 1.0.19+ds-0qli3~bpo14+1~. This was
    filed as #52. First I'm adding a test to it to demonstrate the problem.
    My next change will fix it.
    
    Signed-off-by: Robie Basak <robie.basak@oss.qualcomm.com>

--- lib/next_qcom_version_test.py
+++ lib/next_qcom_version_test.py
@@ -27,6 +27,12 @@ from next_qcom_version import increment_qcom_version
         ("1.2qlibuild1", "qli", "1.3qli"),
         ("1.2qli", "qli", "1.3qli"),
         ("1.0.7qli10~bpo1", "qli", "1.0.7qli10~bpo2"),
+        pytest.param(
+            "1.0.19+ds-0qli3~bpo13+1",
+            "qli",
+            "1.0.19+ds-0qli3~bpo13+2",
+            marks=pytest.mark.xfail(reason="Issue #52"),
+        ),
         # --- qcom migration: qcom suffix replaced by qli ---
         ("1.2qcom1", "qli", "1.2qli2"),
         ("1.2-3qcom1", "qli", "1.2-3qli2"),
